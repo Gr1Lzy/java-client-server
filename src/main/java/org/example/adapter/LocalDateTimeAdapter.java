96d401483fe30df7145ae02edb8488f58aa12c50
Andrii Kolomoiets <kolomoets02@gmail.com>
2024-05-27 16:55:00 +0300

    format time HH:mm:ss:SSSSSS

--- src/main/java/org/example/adapter/LocalDateTimeAdapter.java
+++ src/main/java/org/example/adapter/LocalDateTimeAdapter.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class LocalDateTimeAdapter extends TypeAdapter<LocalDateTime> {
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss:SSSSSS");
 
     @Override
     public void write(JsonWriter jsonWriter, LocalDateTime localDateTime) throws IOException {
